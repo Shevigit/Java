0348e1103d9f93bb8b81200d571726b400aa937b
Your Name <sh0548503408@gmail.com>
2025-05-25 20:29:14 +0300

    merge to checout getRepresentative

--- clientSide/src/Main.java
+++ clientSide/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-    InquiryManagerClient inquiryManagerClient=new InquiryManagerClient("localhost",5000);
+    InquiryManagerClient inquiryManagerClient=new InquiryManagerClient("localhost",9000);
     inquiryManagerClient.execut();
 
 
